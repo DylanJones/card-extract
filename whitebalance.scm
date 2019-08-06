9d68feeddb4ba56c36fcc045c8de16b86b1e3922
Jack Duvall <jediguy9@gmail.com>
2019-08-05 22:04:26 -0400

    Fix GIMP script typo

--- whitebalance.scm
+++ whitebalance.scm
@@ -202,11 +202,11 @@
 
 (define (script-fu-whiten-file filename)
   (let* (
-      (image (car (gimp-file-load RUN-NOINTERACTIVE filename filename)))
+      (image (car (gimp-file-load RUN-NONINTERACTIVE filename filename)))
       (drawable (car (gimp-image-get-active-layer image)))
     )
     (script-fu-whiten-image image drawable)
-    (gimp-file-save RUN-NOINTERACTIVE image drawable filename filename)
+    (gimp-file-save RUN-NONINTERACTIVE image drawable filename filename)
     (gimp-image-delete image)
   )
 )
